b2b8fd7c1301735950114b230ab6158b5a543174
Shabda Raaj <shabda@agiliq.com>
2010-09-24 15:57:39 +0530

    Setup.py for Pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+import ez_setup
+ez_setup.use_setuptools()
+from setuptools import setup, find_packages
+setup(
+    name = "django-pastebin",
+    version = "0.1",
+    packages = find_packages(),
+    author = "Agiliq and friends",
+    author_email = "shabda@agiliq.com", 
+    description = "A django based pastebin",
+    url = "http://github.com/agiliq/django-pastebin",
+    include_package_data = True
+)
