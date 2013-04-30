70b5e1f1fd048862b156ec97f5a95fa967fcc8da
krvc <krvc.it@gmail.com>
2013-05-01 00:37:07 +0530

    Modified settings.py

--- settings.py
+++ settings.py
@@ -69,7 +69,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    "/home/rakhi/code/pastebin/django-pastebin/static"
+    "/home/rakhi/code/pastebin/django-pastebin/static",
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -121,6 +121,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
