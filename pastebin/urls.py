f78fecb98e52cf6c2bc8b8f4926cd17c2d9a8bdb
krvc <krvc.it@gmail.com>
2013-05-01 00:40:37 +0530

    Fixed url pattern in pastebin/urls.py

--- pastebin/urls.py
+++ pastebin/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import *
 from django.conf import settings
 from django.conf.urls import patterns
 from django.views.generic import TemplateView
 
 urlpatterns = patterns('pastebin.views',
         url(r'^$', 'index', name='djpaste_index'),
-        url(r'^$', TemplateView.as_view(template_name='djpaste/help.html'), name='djpaste_help'),
+        url(r'^help/$', TemplateView.as_view(template_name='djpaste/help.html'), name='djpaste_help'),
         url(r'^paste/(?P<id>\d+)/$', 'paste_details', name='djpaste_paste_details'),
         url(r'^plain/(?P<id>\d+)/$', 'plain', name='djpaste_plain'),
         url(r'^html/(?P<id>\d+)/$', 'html', name='djpaste_html'),
