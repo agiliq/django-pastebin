25815c4adc5f68d6d0040d111945c3276b040fdc
krvc <krvc.it@gmail.com>
2013-05-02 04:19:35 +0530

    updated files

--- pastebin/settings.py
+++ pastebin/settings.py
@@ -68,7 +68,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = "%s/static_media" % proj_dir
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -77,7 +77,7 @@ STATIC_URL = '/static/'
 # Additional locations of static files
 STATICFILES_DIRS = (
     
-    "%s/static/" % proj_dir,
+    "%s/static" % proj_dir,
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
