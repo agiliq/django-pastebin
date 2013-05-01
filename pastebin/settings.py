e279a8704d047548586be732afe7ad800f71ef3f
krvc <krvc.it@gmail.com>
2013-05-02 01:19:34 +0530

    changed static dir

--- pastebin/settings.py
+++ pastebin/settings.py
@@ -69,7 +69,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    "/home/rakhi/code/pastebin/django-pastebin/static",
+    "static/",
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
