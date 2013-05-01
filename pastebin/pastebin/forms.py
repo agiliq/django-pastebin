fa60d500c8c7f592ac43dff1ce1b48099c76ce5d
krvc <krvc.it@gmail.com>
2013-05-02 00:57:29 +0530

    added django app

---
+++ pastebin/pastebin/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import CodePaste
+
+
+class PasteForm(forms.Form):
+    lang_names = (('Python', 'Python', ), ('Perl', 'Perl',), ('Ruby', 'Ruby',), ('PythonConsole', 'Python Console'), \
+        ('RubyConsole', 'Ruby Console'), ('PythonTraceback', 'Python Traceback'), ('HtmlDjango','Django Template'),\
+        ('Html', 'Html'), ('Others', 'Others'))
+    text = forms.CharField(label='Code', widget=forms.Textarea(attrs = {"rows":20, "cols":80}))
+    language = forms.ChoiceField(lang_names, required = False)
+    title = forms.CharField(required = False, max_length=60, widget = forms.TextInput(attrs = {'size':50}))
+    name = forms.CharField( max_length=60, widget = forms.TextInput(attrs = {'size':50}), required = False)
+    def save(self):
+        snippet = CodePaste(text = self.cleaned_data['text'], language=self.cleaned_data['language'], title=self.cleaned_data['title'], name=self.cleaned_data['name'])
+        snippet.save()
+        return snippet
---
+++ pastebin/pastebin/urls.py
@@ -0,0 +1,26 @@
+from django.conf.urls import *
+from django.conf import settings
+from django.conf.urls import patterns
+from django.views.generic import TemplateView
+
+from .views import FormIndex
+from .views import PasteDetails
+from .views import Plain
+from .views import Html
+
+urlpatterns = patterns('pastebin.views',
+        url(r'^$', 'index', name='djpaste_index'),
+        url(r'^help/$', TemplateView.as_view(template_name='djpaste/help.html'), name='djpaste_help'),
+        url(r'^paste/(?P<id>\d+)/$', 'paste_details', name='djpaste_paste_details'),
+        url(r'^plain/(?P<id>\d+)/$', 'plain', name='djpaste_plain'),
+        url(r'^html/(?P<id>\d+)/$', 'html', name='djpaste_html'),
+
+
+)
+
+if settings.DEBUG:    
+    media_dir = settings.MEDIA_ROOT
+    urlpatterns += patterns('',
+            url(r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': media_dir}),
+        )
+
