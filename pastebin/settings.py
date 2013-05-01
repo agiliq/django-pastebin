4e18a1eb7daef9a90d53fa7a2c5ea5f5cabbf74d
krvc <krvc.it@gmail.com>
2013-05-02 02:05:24 +0530

    changed db settings

--- pastebin/settings.py
+++ pastebin/settings.py
@@ -168,16 +168,5 @@ except ImportError:
     print "Missing localsetting"
     raise
 
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-
-dbconfig = dj_database_url.config()
-if dbconfig:
-    DATABASES['default'] =  dbconfig
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-
 
 
