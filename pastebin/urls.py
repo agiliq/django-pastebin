e392cbdaff10b5fe8bbecc9ab3b78841abd1ffb9
krvc <krvc.it@gmail.com>
2013-05-03 03:17:26 +0530

    pep8 fixes

--- pastebin/urls.py
+++ pastebin/urls.py
@@ -1,10 +1,11 @@
-from django.conf.urls import patterns,url
+from django.conf.urls import patterns, url
 from django.conf import settings
 from django.views.generic import TemplateView
 
 urlpatterns = patterns('pastebin.views',
         url(r'^$', 'index', name='djpaste_index'),
-        url(r'^help/$', TemplateView.as_view(template_name='djpaste/help.html'), name='djpaste_help'),
+        url(r'^help/$', TemplateView.as_view(template_name='djpaste/help.html'), 
+        									name='djpaste_help'),
         url(r'^paste/(?P<id>\d+)/$', 'paste_details', name='djpaste_paste_details'),
         url(r'^plain/(?P<id>\d+)/$', 'plain', name='djpaste_plain'),
         url(r'^html/(?P<id>\d+)/$', 'html', name='djpaste_html'),
